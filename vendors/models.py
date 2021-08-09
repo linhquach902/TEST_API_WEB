0f359f10ed28e854dcb33971d8e6ef68887991bc
linhquach <linhquach902@gmail.com>
2021-08-09 16:55:17 +0700

    update database 2

--- vendors/models.py
+++ vendors/models.py
@@ -10,6 +10,7 @@ class Vendor(models.Model):
     phone = models.CharField(max_length=20)
     vendor_email = models.CharField(max_length=100)
     company_type = models.CharField(max_length=200)
+    accept_card = models.CharField(max_length=50,null=True)
 
     def get_absolute_url (self):
         return reverse('vendor_detail', args=[str(self.id)])
