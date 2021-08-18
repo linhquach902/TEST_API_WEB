637ca36661466ebf3c12cac81e0ae5a717aa1358
linhquach <linhquach902@gmail.com>
2021-08-18 14:39:14 +0700

    update human database 2

--- human/models.py
+++ human/models.py
@@ -7,6 +7,8 @@ class Human(models.Model):
     CMND = models.CharField(max_length=50)
     ten = models.CharField(max_length=50)
     ngay_sinh = models.DateField()
+    gioi_tinh = models.CharField(max_length=50,null=True)
+    dia_chi = models.CharField(max_length=100,null=True)
     loai_vac_xin = models.CharField(max_length=50)
     ngay_tiem = models.DateField()
     so_lan_tiem = models.IntegerField()
